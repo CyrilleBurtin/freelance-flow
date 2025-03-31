bae13e5d739b998360675a1f9508118f3fbcb17b
Cyrille <cyrille.burtin@me.com>
2025-03-31 19:07:18 +0200

    [feat] add sign in and sign up comp with chadcn comp and prettier

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,31 @@
-import { betterAuth } from "better-auth";
-import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { betterAuth } from 'better-auth';
 
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
-    provider: "postgresql"
-  }),
-});
+  emailAndPassword: {
+    enabled: true,
+    async sendResetPassword(data, request) {
+      // Send an email to the user with a link to reset their password
+    },
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+    gitlab: {
+      clientId: process.env.GITLAB_CLIENT_ID!,
+      clientSecret: process.env.GITLAB_CLIENT_SECRET!,
+    },
+    linkedin: {
+      clientId: process.env.LINKEDIN_CLIENT_ID!,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET!,
+    },
+  },
 
+  /** if no database is provided, the user data will be stored in memory.
+   * Make sure to provide a database to persist user data **/
+});
